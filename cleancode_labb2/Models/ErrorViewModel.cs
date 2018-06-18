89d06ae53b56ad451c19ba5a9dbb466a0e8e0970
eboxy55 <eboxy55@gmail.com>
2018-06-18 21:05:30 +0200

    Add project files.

---
+++ cleancode_labb2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cleancode_labb2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
